61c381fa3d8c480ec7066706e56270975fdde579
smhrd <smhrd@59.0.129.175>
2022-01-14 00:18:38 +0900

    궁금

--- miniProject/src/miniProject/main.java
+++ miniProject/src/miniProject/main.java
@@ -3,8 +3,8 @@ package miniProject;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("미니프로젝트 시작합니다.");
+System.out.println("어떻게하는걸까...");
 	}
 
 }
